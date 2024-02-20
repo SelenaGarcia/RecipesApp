e1374d7d1c6ea99f0a21765c11ff760c33d8e73c
Selena Garcia Lobo <selenagarcialobo@gmail.com>
2024-02-20 00:08:56 -0300

    Initial Commit

---
+++ RecipesApp/RecipesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesAppApp.swift
+//  RecipesApp
+//
+//  Created by Selena García Lobo on 20/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
